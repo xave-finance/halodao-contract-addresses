b5541bf19487c60dcd3b8904583fe4ccf8a0e67b
Tracy <teresa.arciaga@gmail.com>
2022-06-21 09:41:03 +0800

    Add lending market addresses for Polygon

--- src/matic.ts
+++ src/matic.ts
@@ -45,7 +45,48 @@ const addresses: AddressCollection = {
       EURS: '0x73366Fe0AA0Ded304479862808e02506FE556a98'
     }
   },
-  tokens
+  tokens,
+  lendingMarket: {
+    protocol: {
+      lendingPoolAddressesProvider:
+        '0x30ce25f499A6826b5f60c1EdeE649119FbDABb4a',
+      lendingPoolAddressesProviderRegistry:
+        '0x4bf6c7dD88B167b310d8Ca8BeEdd8F6A80647B3d',
+      reserveLogic: '0x06EEDD74387fBd3FB7165aB39fda1beD3fead536',
+      genericLogic: '0xE3eef400C835dfB0Ffb8dA5cE925492bE06f9c4c',
+      validationLogic: '0x6D3dB03f082802037D1F44A5b12813903AFf13C9',
+      lendingPool: '0x023f2B1Ea0F8e862A29C2F5ca73E40F45c23d540',
+      lendingPoolConfiguratorImpl: '0x587dA1dBdb1A8B801A4b04c4dCB36B8d37DEcACE',
+      lendingPoolConfigurator: '0x41913b1038714584Efc0624bA3CE84eC8cd515A1',
+      lendingPoolProxy: '0x023f2B1Ea0F8e862A29C2F5ca73E40F45c23d540',
+      stableAndVariableTokensHelper:
+        '0xe56415e89d6fC48B6037527d8Ee43dE12c4B29B4',
+      aTokensAndRatesHelper: '0x8A898d7423D9785495Fac2C69f0cC4eFE36Bff12',
+      aaveOracle: '0xFf4b5a92240CBaa0384f3d7f5e2D50772A80c42E',
+      lendingRateOracle: '0x5B9F536650caCDBA9c7957c8a35e6F107b71cd69',
+      aaveProtocolDataProvider: '0xC9D2C44937783DaD39c47c352ea0FA191C6db0af',
+      stableDebtToken: '0x28408A6fCb70268f3CD7a3af384942DD596fAe27',
+      variableDebtToken: '0xd660450913D8D41b5090B252cCe8b67A92367dA1',
+      aToken: '0xe5778F0EA12ad5b729B5AC119ac215507100B2Ee',
+      defaultReserveInterestRateStrategy:
+        '0xa517CC983716d9514983319271ccDe4a3B43fDAE',
+      wethGateway: '0x8Bfa7b45Ad86df7BeD67E91A676b7495B0402d04',
+      lendingPoolCollateralManager:
+        '0x2EDd5a286255764415816d1B709F961a3E7E1101',
+      walletBalanceProvider: '0x3450A01F5fABe250d4443Fcd7Be7c3CcA524C266',
+      rnbwIncentivesController: '0x390420010cc3244219f5D5b4d63Da7DC125AE90C',
+      treasury: '0x74042CE43D1e74FF5627B1e9ff2ee950251b5Db4',
+      uiIncentiveDataProvider: '0x099C76Ec7B2B0dA33Ce9cFb6D3e5383bBba1da87',
+      uiHaloPoolDataProvider: '0xB3600b3a54ef2D294Bcd6778CD1Efc0952365415'
+    },
+    lpAssets: {},
+    priceOracles: {
+      fxPHP: '0x24eA470A0836B5D24d82fEf1f55ad4C79DFd0b04',
+      TAGPHP: '0x24eA470A0836B5D24d82fEf1f55ad4C79DFd0b04',
+      XSGD: '0x22070511b8985C8694413847ed81E6A856d27D33'
+    },
+    wrappedNativeToken: '0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889'
+  }
 }
 
 export default addresses
