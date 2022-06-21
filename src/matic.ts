20450d72ee0f199fc6a4d5c3bb9323260a1e0f78
Tracy <teresa.arciaga@gmail.com>
2022-06-21 10:58:51 +0800

    Fix typo for wrappedNativeToken in matic

--- src/matic.ts
+++ src/matic.ts
@@ -85,7 +85,7 @@ const addresses: AddressCollection = {
       TAGPHP: '0x24eA470A0836B5D24d82fEf1f55ad4C79DFd0b04',
       XSGD: '0x22070511b8985C8694413847ed81E6A856d27D33'
     },
-    wrappedNativeToken: '0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889'
+    wrappedNativeToken: '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270'
   }
 }
 
