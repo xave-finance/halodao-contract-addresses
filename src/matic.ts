337c4cd2149c9b6923bb5ce1a325f63b7dff6bdb
aplki <aplki@protonmail.com>
2022-08-30 14:30:40 +0800

    feat: polygon lending market support

--- src/matic.ts
+++ src/matic.ts
@@ -44,6 +44,48 @@ const addresses: AddressCollection = {
       ]
     }
   },
+
+  lendingMarket: {
+    protocol: {
+      lendingPoolAddressesProvider:
+        '0x1D5da10918CAC00a62a22532881B9c83C1C9551a',
+      lendingPoolAddressesProviderRegistry:
+        '0x86a9cd9489B5D33c1eC10fdccCaBC3555cBfAAD2',
+      reserveLogic: '0xaf133753F37694cD4b9aD2A886bCb2eb76CDfa51',
+      genericLogic: '0x434A125D77218589014aA60900CAE870ed9BcDA8',
+      validationLogic: '0x539b12ADAD0f5fC6b2365657f8a4669c0e3Eb7Ab',
+      lendingPoolImpl: '0x3f51BAd685B072Ee5313Fc02F1EFdeFa878618D3',
+      lendingPool: '0x51485c7278fa37f0f504F541f2755abda7715abf',
+      lendingPoolProxy: '0x51485c7278fa37f0f504F541f2755abda7715abf',
+      lendingPoolConfiguratorImpl: '0x63d51fcefDf487693b59e8Be04Faa8DE6837f630',
+      lendingPoolConfigurator: '0xBacdf66F391E9CB65C1D7CD3e506b6AEE5fA2F88',
+      stableAndVariableTokensHelper:
+        '0x60A05D525c85f1Fe07e7bC32658B489FE69e805b',
+      aTokensAndRatesHelper: '0x3aAfd370FFcA5B1EEC4875d3EdC3939e5f9004a1',
+      aaveOracle: '0x2159Cdc79C36dffFb03296FD84Db45b0b932D91E',
+      lendingRateOracle: '0x5AE7Ef3118D3F8739BA5525534367757FEd03540',
+      aaveProtocolDataProvider: '0x10B71123E0B116E33277079b33d045a23cD21a4D',
+      stableDebtToken: '0xEf1Ba1BBC37d9A8e2FF4CFd0f82E6F290E81016C',
+      variableDebtToken: '0x844461b5f3Fb323B9f000DF4Eb8d446A1ceBF6aC',
+      aToken: '0xaAA14D6559d4b8091759824186999c6f0AD592dE',
+      defaultReserveInterestRateStrategy:
+        '0xF894fA826926DF73ef43D10272Fb97102dA957C4',
+      wethGateway: '0x679f321BAdD89a2037521a9611A35BAdDAAdF6Ad',
+      lendingPoolCollateralManager:
+        '0xeEc23b994F27B6914A06180c6B5FA4f83ec80712',
+      walletBalanceProvider: '0x1Fc9639c1eDe7ba2d33E609A0F2956A23eF99E3e',
+      rnbwIncentivesController: '0x', // not available
+      treasury: '0x5560659d9a4aB330dE2112fc8Ee0989857197728', // multisig
+      uiIncentiveDataProvider: '0xF94052eB28149212e5D814cE75d45586bAceE4ca',
+      uiHaloPoolDataProvider: '0x7B007AC2B082E81542CfC74dA327E4bbFd5B9904'
+    },
+    lpAssets: {
+      LP_XSGD_USDC: fxPools.LP_XSGD_USDC
+    },
+    priceOracles: {
+      LP_XSGD_USDC: '0xbca5c841eC9cC6Bd54ee18450eAe3B4D7b68146b'
+    }
+  },
   ammV2: {
     vault: '0xBA12222222228d8Ba445958a75a0704d566BF2C8',
     pools: {
