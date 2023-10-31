6de71d314de7fa47199bb1f62f8bc18d3212f637
andreiashu <andreiashu@gmail.com>
2023-10-31 16:32:40 +0700

    🍇 GBPT token

--- src/types.ts
+++ src/types.ts
@@ -159,6 +159,7 @@ export type AddressCollection = {
       BRLA?: string
       VNXAU?: string
       VEUR?: string
+      GBP?: string
       'bb-a-usd'?: string
       PHPT?: string
       PHPX?: string
